c6f16545339b91982016285956baaa748056732b
aalperaktass <89644211+aalperaktass@users.noreply.github.com>
2022-11-05 04:53:12 +0300

    Çeşitli veri taşıma yöntemleri ve metodları ile ilgili çalışmalar yapıldı.

--- MyAspNetCoreApp.Web/Controllers/OrnekController.cs
+++ MyAspNetCoreApp.Web/Controllers/OrnekController.cs
@@ -4,13 +4,48 @@ namespace MyAspNetCoreApp.Web.Controllers
 {
     public class OrnekController : Controller
     {
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
         public IActionResult Index()
         {
+
+            ViewBag.name = "Alper";
+            ViewData["Age"] = 24; 
+            ViewData["names"] = new List<string>() {"Alper",  "Ali",  "Ezgi" };
+
+            TempData["Ad"] = "Alper";
+            TempData["Soyad"] = "Aktaş";
             return View();
+        }
+        public IActionResult Index2()
+        {
+            var productList = new List<Product>()
+            {
+                new(){Id=1,Name="Alper"},
+                new(){Id=2,Name="Ali"},
+                new(){Id=3,Name="Ezgi"}
+            };
+            return View(productList);
+        }
+        public IActionResult Index3()
+        {
+            return RedirectToAction("Index", "Ornek");
         }
         public IActionResult ContentResult()
         {
             return Content("Alper");
+        }
+        public IActionResult ParametreView(int id)
+        {
+            return RedirectToAction("JsonResultParametre", "ornek", new { id = id});
+        }
+        public IActionResult JsonResultParametre(int id)
+        {
+           return Json(new { Id = 3 });
+
         }
         public IActionResult JsonResult() 
         {
